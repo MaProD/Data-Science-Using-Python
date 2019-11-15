0149053b0ba18edc6f51161883e13e07fe19b107
Mathematics and Programming For Development - Murang'a University of Technology <57797174+MaProD@users.noreply.github.com>
2019-11-15 16:05:18 +0300

    Update visualize.py

---
+++ visualize.py
@@ -0,0 +1,17 @@
+Learn more or give us feedback
+#Everything  starting with "#" is a comment and is ignored by compilers.
+#Pairplots using Iris.csv dataset.
+#Importing libraries
+import numpy as np 
+import pandas as pd  
+import seaborn as sns 
+from matplotlib import pyplot  as plt
+%matplotlib inline
+#reading your data set.
+data = pd.read_csv("Iris.csv")
+#Printing the first 5 data  rows.
+data.head()
+
+plt.hist(data.SepalLengthCm)
+#Showing your plot
+plt.show()
